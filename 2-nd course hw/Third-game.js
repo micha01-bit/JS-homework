2d48a24c3b74c13842588ea9a97e4a13231756f3
micha01-bit <ebespalov05@gmail.com>
2025-03-21 11:41:14 +0700

    hw-7

---
+++ 2-nd course hw/Third-game.js
@@ -0,0 +1,8 @@
+function reverseText() {
+    const inputText = prompt("Введите текст для переворота:");
+    if (inputText !== null) {
+      const reversedText = inputText.split('').reverse().join('');
+      alert("Перевернутый текст: " + reversedText);
+    }
+  }
+  
